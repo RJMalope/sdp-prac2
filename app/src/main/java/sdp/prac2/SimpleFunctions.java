6041c67613b830fc416bf6f19235c0ebd28bb66f
Anganathi Manjingolo <G21M3441@ict.ru.ac.za>
2025-04-12 18:36:07 +0200

    Added the method Task4, which takes 2 lists and mirror multiplies the elements and returns the resultant list, to SimpleFunctions.java

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,21 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+
+    public List<Integer> Task4(List<Integer> a, List<Integer> b) {
+        if (a.size() != b.size()) {
+            return null;
+        }
+
+        int listSize = a.size();
+        List<Integer> result = new ArrayList<Integer>();
+
+        for (int i = 0; i < listSize; i++) {
+            int prod = a.get(i) * b.get(listSize - i - 1);
+            result.add(prod);
+        }
+        
+        return result;
+    }
+    
 }
